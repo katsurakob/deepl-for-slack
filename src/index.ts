9388e5da86e1f084284892ab6601fe6cf47552be
Katsura Kobayashi <katsurakob@gmail.com>
2021-02-08 12:50:21 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ middleware.enableAll(app);
 // -----------------------------
 
 app.shortcut("deepl-translation", async ({ ack, body, client }) => {
-  // await ack();
+  await ack();
   await runner.openModal(client, body.trigger_id);
 });
 
